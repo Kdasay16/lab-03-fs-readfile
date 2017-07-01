a99629482315dcf2cc9fa0cd1fabc35c7603e40f
Kayla Asay <kdasay16@gmail.com>
2017-06-30 23:53:55 -0700

    testing

--- lab-kayla/test.js
+++ lab-kayla/test.js
@@ -1,13 +1,14 @@
 'use strict';
 
-const expect = require('chai').expect;
-const fs = require('fs');
+module.exports = exports = {};
+// const expect = require('chai').expect;
+// const fs = require('fs');
 
 describe('lib', function() {
   describe('#readFile', function() {
-    it('should read files and produce hex')
-    let one = `${__dirname}/dataFile/dataOne.txt`;
-    let two = `${__dirname}/dataFile/dataTwo.txt`;
-    let tree = `${__dirname}/dataFile/dataThree.txt`;
+    it('should read files and produce hex');
+    // let one = `${__dirname}/dataFile/dataOne.txt`;
+    // let two = `${__dirname}/dataFile/dataTwo.txt`;
+    // let tree = `${__dirname}/dataFile/dataThree.txt`;
   });
 });
